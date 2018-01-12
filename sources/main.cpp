7df6703f3c829472cf5a4eb45b6713a8b2d54f94
M1SIAM INP Didier <TMP-Didier@F215-06.e-im2ag.ujf-grenoble.fr>
2018-01-12 15:07:00 +0100

    noche

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,53 +3,19 @@
 #include "starter_1.h"
 
 using namespace cv;
+using std::cout;
+using std::endl;
 
-
-Mat symetry_y(Mat &image) {
-    /* returns the symetry along the y axis */
-    int nRows = image.rows;
-    int nCols = image.cols;
-    Mat image_sym;
-    //creation of a Mat with the same size as 'image'
-    image_sym.create(nRows, nCols, CV_8UC1);
-    for (uint i = 0; i < nCols; ++i) {
-      for (uint j = 0; j < nRows; ++j) {
-        //gets the intensity value of image
-        Scalar intensity = image.at<uchar>(j, i);
-        //fills 'image_sym' according to the symetry
-        image_sym.at<uchar>(j, nCols - i) = intensity[0];
-      }
-    }
-    return(image_sym);
-}
-
-Mat symetry_diag(Mat &image) {
-    /* returns the symetry along the x/y diagonal */
-    int nRows = image.rows;
-    int nCols = image.cols;
-    Mat image_sym;
-    //creation of a Mat with the same size as 'image'
-    image_sym.create(nCols, nRows, CV_8UC1);
-    for (uint i = 0; i < nCols; ++i) {
-      for (uint j = 0; j < nRows; ++j) {
-        //gets the intensity value of image
-        Scalar intensity = image.at<uchar>(j, i);
-        //fills 'image_sym' according to the symetry
-        image_sym.at<uchar>(i, j) = intensity[0];
-      }
-    }
-    return(image_sym);
-}
 
 int main(int argc, char** argv )
 {
-    if ( argc != 2 )
+    if ( argc < 2 )
     {
-        printf("usage: DisplayImage.out <Image_Path>\n");
+        printf("usage: Digital_anlysis <Image_Path> [Test]\n");
         return -1;
     }
     Mat image;
-    image = imread(argv[1], 0);
+    image = imread(argv[1], 1);
     if (!image.data)
     {
         printf("No image data \n");
@@ -58,17 +24,23 @@ int main(int argc, char** argv )
     namedWindow("Display Image", WINDOW_AUTOSIZE );
     imshow("Display Image", image);
     waitKey(0);
-    //cvtColor(image, grey_image, COLOR_BGR2GRAY);
-    Mat grey_image = convert_to_float(image);
-    std::cout << grey_image << std::endl;
-    /*
-    draw_uniform_rectangle(570,570,7,100,0.0, grey_image);
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", grey_image);
+    image = convert_to_float(image);
+    //cvtColor(image, image, COLOR_BGR2GRAY);
+    imshow("Display Image", image);
     waitKey(0);
+
+
+
+    Rect r (10,570,7,100);
+    draw_uniform_rectangle(image, r, 1.0);
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+    waitKey(0);
+
     Mat image_sym = symetry_diag(image);
-    std::cout << image_sym << std::endl;
+    imshow("Display Image", image_sym);
+    waitKey(0);
     imwrite("sym.png", image_sym);
-    */
+
     return 0;
 }
--- sources/starter_1.h
+++ sources/starter_1.h
@@ -8,15 +8,51 @@ using namespace cv;
 Mat convert_to_float(Mat image){
   Mat dst;
   image.convertTo(dst, CV_32F, 1.0/255.0);
-  return(dst);
+  return dst;
 }
 
-int draw_uniform_rectangle (uint x, uint y, uint sx, uint sy, float color, Mat image){
-  assert ((x+sx <= image.cols) && (y+sy <= image.rows));
-  Rect r(x,y,sx,sy);
+int draw_uniform_rectangle (Mat image, Rect r, float color){
+  //assert ((x+sx <= image.cols) && (y+sy <= image.rows));
   image = image(r);
   image = Scalar(color);
   return 0;
 }
+
+Mat symetry_y(Mat &image) {
+    /* returns the symetry along the y axis */
+    int nRows = image.rows;
+    int nCols = image.cols;
+    Mat image_sym;
+    //creation of a Mat with the same size as 'image'
+    image_sym.create(nRows, nCols, CV_32F);
+    for (uint i = 0; i < nCols; ++i) {
+      for (uint j = 0; j < nRows; ++j) {
+        //gets the intensity value of image
+        Scalar intensity = image.at<float>(j, i);
+        //fills 'image_sym' according to the symetry
+        image_sym.at<float>(j, nCols - i) = intensity[0];
+      }
+    }
+    return(image_sym);
+}
+
+Mat symetry_diag(Mat &image) {
+    /* returns the symetry along the x/y diagonal */
+    int nRows = image.rows;
+    int nCols = image.cols;
+    Mat image_sym;
+    //creation of a Mat with the same size as 'image'
+    image_sym.create(nCols, nRows, CV_32F);
+    for (uint i = 0; i < nCols; ++i) {
+      for (uint j = 0; j < nRows; ++j) {
+        //gets the intensity value of image
+        Scalar intensity = image.at<float>(j, i);
+        //fills 'image_sym' according to the symetry
+        image_sym.at<float>(i, j) = intensity[0];
+      }
+    }
+    return(image_sym);
+}
+
 
 #endif
