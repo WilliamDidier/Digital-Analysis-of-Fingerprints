e1fc0e9e22476fb0fc523be53a6ff19c837ce9e7
M1SIAM INP Couderc <TMP-Couderc@F215-07.e-im2ag.ujf-grenoble.fr>
2018-01-12 14:10:23 +0100

    hum

--- sources/main.cpp
+++ sources/main.cpp
@@ -49,7 +49,7 @@ int main(int argc, char** argv )
         return -1;
     }
     Mat image;
-    image = imread(argv[1], 0);
+    image = imread(argv[1], 1);
     if (!image.data)
     {
         printf("No image data \n");
@@ -60,12 +60,12 @@ int main(int argc, char** argv )
     waitKey(0);
     Mat grey_image;
     cvtColor(image, grey_image, COLOR_BGR2GRAY);
-    convert_to_float(grey_image);
-    draw_uniform_rectangle(570,570,7,100,0.0, grey_image);
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", grey_image);
-    waitKey(0);
-    Mat image_sym = symetry_diag(image);
+    // convert_to_float(grey_image);
+    // draw_uniform_rectangle(570,570,7,100,0.0, grey_image);
+    // namedWindow("Display Image", WINDOW_AUTOSIZE );
+    // imshow("Display Image", grey_image);
+    // waitKey(0);
+    Mat image_sym = symetry_diag(grey_image);
     //std::cout << image_sym << std::endl;
     imwrite("sym.png", image_sym);
     return 0;
