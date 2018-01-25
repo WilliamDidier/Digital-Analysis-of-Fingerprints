a661c5de2ef7d5ba75af14c17b7c5d2cecb50ef4
M1SIAM INP Couderc <TMP-Couderc@F212-12.e-im2ag.ujf-grenoble.fr>
2018-01-25 16:02:16 +0100

    différentes interpolations

--- src/headers/starter2.h
+++ src/headers/starter2.h
@@ -14,11 +14,11 @@ contains the functions related to the starter 2
 #ifndef STARTER2
 #define STARTER2
 
-vector<int> rotation(int i, int j, int angle );
+vector<int> rotation(int centre_x, int centre_y, int i, int j, int angle );
 
-Mat rotate_img_from_source(Mat image, int angle);
+Mat rotate_img_from_source(Mat image, int angle, int centre_x, int centre_y);
 
-Mat rotate_img_to_dest(Mat image, int angle);
+Mat rotate_img_to_dest(Mat image, int angle, int centre_x, int centre_y);
 
 Mat interpolation_moy_16(Mat image);
 
@@ -37,6 +37,18 @@ vector<float> rotation_cubic( int i, int j, int angle );
 Mat rotate_img_from_source_bilinear(Mat image, int angle);
 
 vector<float> rotation_bilinear( int i, int j, int angle );
+
+float derive_x(float x, float y, Mat image);
+
+float derive_y(float x, float y, Mat image);
+
+float derive_xy(float x, float y, Mat image);
+
+Mat coeff_bicubic(float x, float y, Mat image);
+
+float cubic_interpolation(float x, float y, Mat image);
+
+Mat rotate_img_from_source_bicubic(Mat image, int angle);
 
 
 
