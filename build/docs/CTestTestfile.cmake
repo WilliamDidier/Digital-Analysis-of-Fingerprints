af81fe4386d7b27086fadf6d0447ce9b384b6774
Théo Moins <theo.moins@nsigma.fr>
2018-01-29 09:46:19 +0100

    marre des trolls qui add le build

--- build/docs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /user/5/.base/coudercr/home/Digital-analysis/docs
-# Build directory: /user/5/.base/coudercr/home/Digital-analysis/build/docs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
