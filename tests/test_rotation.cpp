9287150280b52a94ab7190860f91c85569dc0bdb
M1SIAM INP Couderc <TMP-Couderc@F214-10.e-im2ag.ujf-grenoble.fr>
2018-01-30 14:34:09 +0100

    fin main 2

--- tests/test_rotation.cpp
+++ tests/test_rotation.cpp
@@ -13,7 +13,7 @@ Mat rotate(Mat src, double angle)
     Mat dst;
     Point2f pt(src.cols/2, src.rows/2);
     Mat r = getRotationMatrix2D(pt, angle, 1.0);
-    warpAffine(src, dst, r, Size(src.cols, src.rows), INTER_CUBIC, BORDER_TRANSPARENT,Scalar(0));
+    warpAffine(src, dst, r, Size(src.cols, src.rows), INTER_LINEAR, BORDER_CONSTANT,Scalar(255));
     return dst;
 }
 
@@ -37,20 +37,21 @@ int main(int argc, char** argv )
     cout << "Rotation of image...";
     convert_to_float(image, image);
 
-    float angle = 15;
-
+    float angle = -20;
+    Mat rotation_to_dest = rotate_img_to_dest(image, angle);
     Mat rotation_weighted = rotate_img_from_source_weighted(image, angle);
     Mat rotation_from_source_bicubic = rotate_img_from_source_bicubic(image, angle);
     Mat rotation_from_source = rotate_img_from_source(image, angle);
     Mat rotation_from_source_bilinear = rotate_img_from_source_bilinear(image, angle);
     Mat true_rotation = rotate(image, angle);
+    convert_to_int(rotation_to_dest, rotation_to_dest);
     convert_to_int(rotation_from_source, rotation_from_source);
     convert_to_int(rotation_from_source_bicubic, rotation_from_source_bicubic);
     convert_to_int(rotation_from_source_bilinear, rotation_from_source_bilinear);
     convert_to_int(true_rotation, true_rotation);
     convert_to_int(rotation_weighted, rotation_weighted);
     convert_to_int(image, image);
-
+    imwrite("tests/rotation_to_dest.png", rotation_to_dest);
      imwrite("tests/rotation_from_source_bilinear.png", rotation_from_source_bilinear );
      imwrite("tests/rotation_from_source.png", rotation_from_source);
      imwrite("tests/rotation_from_source_bicubic.png", rotation_from_source_bicubic);
